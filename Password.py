e247b57e9fcce9aeb94dff3557c75fafa1b062c9
Suman_Kumar <joewatson881178@gmail.com>
2023-03-20 16:02:39 +0530

    Password Generator  Completed

--- Password.py
+++ Password.py
@@ -7,11 +7,29 @@ print("Welcome to the password Generator! ")
 nr_latters = int(input("How many latters would you like in your password \n"))
 nr_symbols = int(input("How many symbols would you like ?\n"))
 nr_number = int(input("How many numbers would you like \n"))
-password = ""
+password = []
+# for char in range(1, nr_latters+1):
+#     password += random.choice(letter)
+# for num in range(1, nr_latters+1):
+#     password += random.choice(number)
+# for sym in range(1, nr_symbols+1):
+#     password += random.choice(symbol)
+# print(password)
+# total_len = len(password)
+# rand = ""
+# for Def in range(1, total_len+1):
+
+#     rand += random.choice(password)
+# print(rand)
+chara = ""
 for char in range(1, nr_latters+1):
     password += random.choice(letter)
-for num in range(1, nr_latters+1):
+for num in range(1, nr_number+1):
     password += random.choice(number)
 for sym in range(1, nr_symbols+1):
     password += random.choice(symbol)
-print(password)
+random.shuffle(password)
+finalPassword = ""
+for char in password:
+    finalPassword += char
+print(f"Your password is {finalPassword}")
